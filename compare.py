2d0e45f8a6b8e61c3bbe93679c26e7335611df11
Oleksandr Drozd <drozdoleksandr@gmail.com>
2018-07-13 21:39:59 +0300

    Production version 1.0.3
    List of changes:
    - copy changes

--- compare.py
+++ compare.py
@@ -4,11 +4,11 @@
 def compare_moves(player, computer):
         if (player == 1 and computer == 1) or (player == 2 and computer == 2) or (
                 player == 3 and computer == 3):
-            return "It's a tie!"
+            return "It's a tie."
 
         elif (player == 1 and computer == 3) or (player == 2 and computer == 1) or (
                     player == 3 and computer == 2):
             return "You win the round!"
 
         else:
-            return "You lose the round!"
+            return "You lose the round :("
